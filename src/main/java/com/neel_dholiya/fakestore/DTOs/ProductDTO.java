d3c7d3c086961da085d6c248d2078771c6003151
Neel Dholiya <neeldholiya04@gmail.com>
2024-03-12 15:01:01 +0530

    Initial commit

---
+++ src/main/java/com/neel_dholiya/fakestore/DTOs/ProductDTO.java
@@ -0,0 +1,10 @@
+package com.neel_dholiya.fakestore.DTOs;
+
+public class ProductDTO {
+    private long id;
+    private String title;
+    private double price;
+    private String category;
+    private String description;
+    private String image;
+}
---
+++ src/main/java/com/neel_dholiya/fakestore/Models/Category.java
@@ -0,0 +1,12 @@
+package com.neel_dholiya.fakestore.Models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+    private long id;
+    private String name;
+}
